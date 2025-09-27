01aa8fc83a74c243da1cd4a072b353e20df505ee
r181104 <sten181104@gmail.com>
2025-09-27 21:07:37 +0530

    s

--- nixos/modules/graphics.nix
+++ nixos/modules/graphics.nix
@@ -4,17 +4,17 @@
   lib,
   ...
 }: {
-  hardware.nvidia.prime = {
-    intelBusId = "PCI:0@0:2:0";
-    nvidiaBusId = "PCI:1@0:0:0";
-    offload.enable = true;
-    offload.enableOffloadCmd = true;
-  };
   hardware.nvidia = {
     open = false;
     nvidiaSettings = true;
     modesetting.enable = true;
     forceFullCompositionPipeline = true;
+  };
+  hardware.nvidia.prime = {
+    intelBusId = "PCI:0@0:2:0";
+    nvidiaBusId = "PCI:1@0:0:0";
+    offload.enable = true;
+    offload.enableOffloadCmd = true;
   };
   boot.extraModulePackages = [config.boot.kernelPackages.nvidia_x11];
   boot.blacklistedKernelModules = ["nouveau" "nvidia_drm" "nvidia_modeset" "nvidia"];
