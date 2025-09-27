78f7bba8bf5146646f7ce184e86f70838d318da0
r181104 <sten181104@gmail.com>
2025-09-27 21:12:40 +0530

    s

--- nixos/modules/graphics.nix
+++ nixos/modules/graphics.nix
@@ -16,6 +16,8 @@
     offload.enable = true;
     offload.enableOffloadCmd = true;
   };
+  hardware.opengl.extraPackages = with pkgs; [vulkan-tools vulkan-loader vulkan-icd-loader];
+  hardware.cpu.intel.updateMicrocode = true;
   boot.extraModulePackages = [config.boot.kernelPackages.nvidia_x11];
   boot.blacklistedKernelModules = ["nouveau" "nvidia_drm" "nvidia_modeset" "nvidia"];
   environment.systemPackages = [pkgs.linuxPackages.nvidia_x11];
