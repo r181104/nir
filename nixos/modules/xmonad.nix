5d2a59fff8a9b093491a342e1c01474ece00bb12
r181104 <sten181104@gmail.com>
2025-09-30 16:55:40 +0530

    s

--- nixos/modules/xmonad.nix
+++ nixos/modules/xmonad.nix
@@ -3,11 +3,15 @@
   pkgs,
   ...
 }: {
-  services.xserver.windowManager.xmonad.enable = true;
-  services.xserver.windowManager.xmonad.enableContrib = true;
-  services.xserver.windowManager.xmonad.extraPackages = with pkgs; [
-    xmobar
-    xterm
-    dmenu
-  ];
+  services.xserver.windowManager.xmonad = {
+    enable = true;
+    enableContribAndExtras = true;
+    enableConfiguredRecompile = true;
+    haskellPackages = pkgs.haskellPackages;
+    extraPackages = haskellPackages: [
+      haskellPackages.xmonad-contrib
+      haskellPackages.monad-logger
+    ];
+  };
+  boot.initrd.systemd.dbus.enable = true;
 }
