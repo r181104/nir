71453bad0aa7850cc83ecffceb2a9647753bf966
r181104 <sten181104@gmail.com>
2025-09-30 22:41:54 +0530

    s

--- nixos/modules/windowmanager.nix
+++ nixos/modules/windowmanager.nix
@@ -16,14 +16,9 @@
     haskellPackages = pkgs.haskellPackages;
   };
   boot.initrd.systemd.dbus.enable = true;
-  # services.xserver.windowManager.bspwm.enable = true;
-  # services.displayManager.defaultSession = "none+bspwm";
+  services.displayManager.defaultSession = "none+bspwm";
   environment.systemPackages = with pkgs; [
-    # # === Window manager & hotkey daemon ===
-    # bspwm
-    # sxhkd
-    #
-    # # === Compositing, notifications, and status bar ===
+    # === Compositing, notifications, and status bar ===
     feh
     picom
     dunst
@@ -31,44 +26,11 @@
     pywal
     bemenu
     betterlockscreen
-    #
-    # # === Icons & themes ===
+    # === Icons & themes ===
     papirus-icon-theme
-    #
-    # # === Input & gestures ===
+    # === Input & gestures ===
     libinput-gestures
     xdotool
-    #
-    # # === Xorg server & utilities ===
-    # xorg.xorgserver
-    # xorg.xorgproto
-    # xorg.xauth
-    # xorg.xinit
-    # xorg.xrandr
-    # xorg.xev
-    # xorg.xset
-    # xorg.xsetroot
-    # xorg.xkbcomp
-    # xorg.xmessage
-    # xorg.xprop
-    # xorg.xwininfo
-    # xorg.xdpyinfo
-    # xorg.xhost
-    # xorg.xmodmap
-    # xorg.xcursorgen
-    # xorg.xf86videointel
-    # xorg.xf86videonouveau
-    # xorg.xf86videoamdgpu
-    # xorg.xf86videoati
-    # xorg.xf86videovesa
-    # xorg.xf86inputlibinput
-    # xorg.libX11.dev
-    # xorg.libXext.dev
-    # xorg.libXrender.dev
-    # xorg.libXrandr.dev
-    # xorg.libXft.dev
-    # xorg.libXi.dev
-    #
     # === GUI tools ===
     lxappearance
   ];
