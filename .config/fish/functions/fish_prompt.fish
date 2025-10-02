16bdb8e0dbdc12ba1fa97dd12e5aef6a3a5f2307
r181104 <sten181104@gmail.com>
2025-10-02 18:44:22 +0530

    changed fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,21 +1,39 @@
 function fish_prompt
-        if not set -q VIRTUAL_ENV_DISABLE_PROMPT
-                set -g VIRTUAL_ENV_DISABLE_PROMPT true
+    set_color cyan
+    echo -n (whoami)"@"(hostname -s)" "
+
+    set_color yellow
+    echo -n (prompt_pwd)" "
+
+    set_color blue
+    set pyfiles *.py
+    if test -f pyproject.toml -o -f requirements.txt -o (count $pyfiles) -gt 0
+        echo -n "Python "(python3 --version 2>/dev/null | string match -r '[0-9.]+')
+    else if test -f Cargo.toml
+        echo -n "Rust "(rustc --version 2>/dev/null | string match -r '[0-9.]+')
+    else if test -f package.json
+        echo -n "Node "(node --version 2>/dev/null)
+    else if test -f go.mod
+        echo -n "Go "(go version 2>/dev/null | string match -r 'go[0-9.]+')
+    end
+    echo -n " "
+
+    set_color green
+    if git rev-parse --is-inside-work-tree 2>/dev/null | grep -q 'true'
+        set branch (git symbolic-ref --short HEAD 2>/dev/null)
+        set dirty (git status --porcelain 2>/dev/null)
+        echo -n "git:("$branch
+        if test -n "$dirty"
+            echo -n " *"
         end
-        set_color yellow
-        printf '%s' $USER
-        set_color normal
-        printf ' in '
-    
-        set_color $fish_color_cwd
-        printf '%s' (prompt_pwd)
-        set_color normal
-    
-        # Line 2
-        echo
-        if test -n "$VIRTUAL_ENV"
-                printf "(%s) " (set_color blue)(path basename $VIRTUAL_ENV)(set_color normal)
-        end
-        printf '✦ '
-        set_color normal
+        echo -n ") "
+    end
+
+    set_color magenta
+    if test $__fish_last_command_duration
+        echo -n "["$__fish_last_command_duration"s] "
+    end
+
+    set_color normal
+    echo -n "> "
 end
